88cb5bec15b06b8ba76ec7a46f0979dbdc3698f0
lluuiissoo <luis.carrazana@live.com>
2021-03-22 23:16:33 -0400

    Fixing package references, adding gitignore, adding config

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/citihub/probr-core
+
+go 1.14
+
+require (
+	github.com/citihub/probr-sdk v0.0.0-20210323022652-263d1384c206
+	github.com/hashicorp/go-hclog v0.14.1
+	github.com/hashicorp/go-plugin v1.4.0
+)
--- main.go
+++ main.go
@@ -6,51 +6,27 @@ import (
 	"os"
 	"os/exec"
 
-	"github.com/citihub/probr-sdk/plugin"
+	plugin "github.com/citihub/probr-sdk/plugin"
 	hclog "github.com/hashicorp/go-hclog"
 	hcplugin "github.com/hashicorp/go-plugin"
 )
 
-// handshakeConfigs are used to just do a basic handshake between
-// a plugin and host. If the handshake fails, a user friendly error is shown.
-// This prevents users from executing bad plugins or executing a plugin
-// directory. It is a UX feature, not a security feature.
-var handshakeConfig_spKubernetes = plugin.HandshakeConfig{
-	ProtocolVersion:  1,
-	MagicCookieKey:   "BASIC_PLUGIN",
-	MagicCookieValue: "probr.servicepack.kubernetes",
-}
-var handshakeConfig_spAPIM = plugin.HandshakeConfig{
-	ProtocolVersion:  1,
-	MagicCookieKey:   "BASIC_PLUGIN",
-	MagicCookieValue: "probr.servicepack.apim",
-}
-var handshakeConfig_spProbr = plugin.HandshakeConfig{
+var handshakeConfig_spKubernetes = hcplugin.HandshakeConfig{
 	ProtocolVersion:  1,
 	MagicCookieKey:   "BASIC_PLUGIN",
 	MagicCookieValue: "probr.servicepack.probr",
 }
 
-// pluginKeyword is used to identify plugin in pluginmap, and to dispense
-var pluginKeyword_spKubernetes = "spKubernetes"
-var pluginKeyword_spAPIM = "spAPIM"
-var pluginKeyword_spProbr = "spProbr"
+// pluginKeyword_spKubernetes is used to identify plugin in pluginmap, and to dispense
+var pluginKeyword_spKubernetes = "spProbr"
 
-// pluginMap is the map of plugins we can dispense.
-var pluginMap_spKubernetes = map[string]plugin.Plugin{
-	pluginKeyword_spKubernetes: &probrsdk.ServicePackPlugin{},
-}
-var pluginMap_spAPIM = map[string]plugin.Plugin{
-	pluginKeyword_spAPIM: &probrsdk.ServicePackPlugin{},
-}
-var pluginMap_spProbr = map[string]plugin.Plugin{
-	pluginKeyword_spProbr: &probrsdk.ServicePackPlugin{},
+// pluginMap_spKubernetes is the map of plugins we can dispense.
+var pluginMap_spKubernetes = map[string]hcplugin.Plugin{
+	pluginKeyword_spKubernetes: &plugin.ServicePackPlugin{},
 }
 
 // Location for plugin binaries
 var pluginPath_spKubernetes = "./servicepacks/kubernetes/kubernetes"
-var pluginPath_spAPIM = "./servicepacks/apim/apim"
-var pluginPath_spProbr = "./cmd/probr"
 
 func main() {
 	// Create an hclog.Logger
@@ -60,16 +36,14 @@ func main() {
 		Level:  hclog.Debug,
 	})
 
-	// TODO: Load all plugins. Get a collection of probrsdk.ServicePack interface.
-	// 	Defer sp.kill
-	//  Execute sp.Greet() for all
-
-	// Kubernetes *********************************************************
 	// We're a host! Start by launching the plugin process.
-	client_spKubernetes := plugin.NewClient(&plugin.ClientConfig{
+	cmd := exec.Command(pluginPath_spKubernetes)
+	cmd.Args = append(cmd.Args, "--varsfile=config.yml")
+	cmd.Args = append(cmd.Args, "--tags=@k-gen")
+	client_spKubernetes := hcplugin.NewClient(&hcplugin.ClientConfig{
 		HandshakeConfig: handshakeConfig_spKubernetes,
 		Plugins:         pluginMap_spKubernetes,
-		Cmd:             exec.Command(pluginPath_spKubernetes),
+		Cmd:             cmd,
 		Logger:          logger,
 	})
 	defer client_spKubernetes.Kill()
@@ -88,77 +62,6 @@ func main() {
 
 	// We should have a ServicePack now! This feels like a normal interface
 	// implementation but is in fact over an RPC connection.
-	spKubernetes := raw_spKubernetes.(probrsdk.ServicePack)
+	spKubernetes := raw_spKubernetes.(plugin.ServicePack)
 	fmt.Println(spKubernetes.Greet())
-	// Kubernetes *********************************************************
-
-	// APIM ***************************************************************
-	// We're a host! Start by launching the plugin process.
-	client_spAPIM := plugin.NewClient(&plugin.ClientConfig{
-		HandshakeConfig: handshakeConfig_spAPIM,
-		Plugins:         pluginMap_spAPIM,
-		Cmd:             exec.Command(pluginPath_spAPIM),
-		Logger:          logger,
-	})
-	defer client_spAPIM.Kill()
-
-	// Connect via RPC
-	rpcClient_spAPIM, err := client_spAPIM.Client()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Request the plugin
-	raw_spAPIM, err := rpcClient_spAPIM.Dispense(pluginKeyword_spAPIM)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// We should have a ServicePack now! This feels like a normal interface
-	// implementation but is in fact over an RPC connection.
-	spAPIM := raw_spAPIM.(probrsdk.ServicePack)
-	fmt.Println(spAPIM.Greet())
-	// APIM ***************************************************************
-
-	// Storage*************************************************************
-	// TODO
-	// Storage*************************************************************
-
-	// Probr **************************************************************
-	// We're a host! Start by launching the plugin process.
-	cmd := exec.Command(pluginPath_spProbr)
-	//cmd.SysProcAttr = &syscall.SysProcAttr{}
-	//cmd.SysProcAttr.CmdLine = fmt.Sprintf("%s %s", pluginPath_spProbr, pluginArgs_spProbr)
-	cmd.Args = append(cmd.Args, "--varsfile=config.yml")
-	cmd.Args = append(cmd.Args, "--tags=@k-gen")
-	client_spProbr := plugin.NewClient(&plugin.ClientConfig{
-		HandshakeConfig: handshakeConfig_spProbr,
-		Plugins:         pluginMap_spProbr,
-		Cmd:             cmd,
-		Logger:          logger,
-	})
-	defer client_spProbr.Kill()
-
-	// Connect via RPC
-	rpcClient_spProbr, err := client_spProbr.Client()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Request the plugin
-	raw_spProbr, err := rpcClient_spProbr.Dispense(pluginKeyword_spProbr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// We should have a ServicePack now! This feels like a normal interface
-	// implementation but is in fact over an RPC connection.
-	spProbr := raw_spProbr.(probrsdk.ServicePack)
-	fmt.Println(spProbr.Greet())
-	// reader := bufio.NewReader(os.Stdin)
-	// fmt.Print("Enter your city: ")
-	// city, _ := reader.ReadString('\n')
-	// fmt.Print("You live in " + city)
-	// fmt.Println(spProbr.Greet())
-	// Probr **************************************************************
 }
